9221cc3412881a72cae2fc6bfab74b8def27e5a6
biruk2116 <brukbela61@gmail.com>
2026-04-08 10:23:14 +0300

    practicing node js export modules

---
+++ Add_local_module.js
@@ -0,0 +1,6 @@
+const add = (a,b) =>{
+    return a*b;
+}
+const multiply=add(23,4)
+
+console.log(multiply)
---
+++ app.js
@@ -0,0 +1,6 @@
+// console.log("Hello Viewers!")
+// require("./Add_local_module.js")
+
+const sub = require("./export_module.js")
+const result=sub(67,12);
+console.log(result)
