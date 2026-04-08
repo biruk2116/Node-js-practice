468b60d51e18c7e3926c9ab3792470488109ed17
biruk2116 <brukbela61@gmail.com>
2026-04-09 02:53:21 +0300

    using parse to display the paths and directories of the file in the form of  json

--- path.js
+++ path.js
@@ -1,7 +1,10 @@
 const path=require("node:path")
 
-console.log(__filename)
-console.log(__dirname)
+// console.log(__filename)
+// console.log(__dirname)
 
-console.log(path.basename(__filename))
-console.log(path.basename(__dirname))
+// console.log(path.basename(__filename))
+// console.log(path.basename(__dirname))
+
+console.log(path.parse(__filename))
+console.log(path.parse(__dirname))
