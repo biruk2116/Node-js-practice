c87272293093309e66dd6ba296a22c3252c1ed12
biruk2116 <brukbela61@gmail.com>
2026-04-09 02:56:17 +0300

    using format  to display the file name and the directory in the formal path way

--- path.js
+++ path.js
@@ -6,5 +6,8 @@ const path=require("node:path")
 // console.log(path.basename(__filename))
 // console.log(path.basename(__dirname))
 
-console.log(path.parse(__filename))
-console.log(path.parse(__dirname))
+// console.log(path.parse(__filename))
+// console.log(path.parse(__dirname))
+
+console.log(path.format(path.parse(__filename)))
+console.log(path.format(path.parse(__dirname)))
