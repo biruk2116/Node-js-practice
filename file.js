564cdb6c8770754ede66c8a3fa5015f4658ce557
biruk2116 <brukbelay61@gmail.com>
2026-05-01 15:00:07 +0300

    refactor file.js: clean up commented code and update unlink functionality

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
-// const {error} =require("node:console");
+           const fs = require("node:fs") 
 
-// const fs = require("node:fs")
+
 
 // const data = fs.readFileSync("./texte.txt","utf-8",(error,data) => {
 //     if(error){
@@ -14,7 +14,7 @@
 // console.log(tex);  
                      //   Creating new file  using writeFile and appendFile
   
-// const fs = require("node:fs")
+
 
 // const data = fs.writeFile("./text1.txt","New file created using node js ",(error) => {
 //     if(error){
@@ -28,14 +28,49 @@
             // Updating file using appendFile and writeFile
 
         
- const fs = require("node:fs")
+//  
 
-const data = fs.appendFile("./text.txt"," adding file in the existing file ",(error) => {
+// const data = fs.appendFile("./text.txt"," adding file in the existing file ",(error) => {
+//     if(error){
+//         console.log(error)
+//     }
+//     else{
+//         console.log("File updated successfully")
+//     }
+// }
+// );
+//              
+
+// const data = fs.writeFile("./text.txt"," Removing and replacing the whole content with new content ",(error) => {
+//     if(error){
+//         console.log(error)
+//     }
+//     else{
+//         console.log("File updated successfully")
+//     }
+// }
+// );
+            //  deleting file using unlink
+
+
+
+const data = fs.unlink("./new.txt",(error) => {
     if(error){
         console.log(error)
     }
     else{
-        console.log("File updated successfully")
+        console.log("File deleted successfully")
     }
 }
 );
+            //  renaming file using rename
+            
+// const data = fs.rename("./delete.txt","./new.txt",(error) => {
+//     if(error){
+//         console.log(error)
+//     }
+//     else{
+//         console.log("File renamed successfully")
+//     }
+// }
+// );
