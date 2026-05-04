138470dd4c9a43d930a567173e191a30462ad92f
biruk2116 <brukbelay61@gmail.com>
2026-05-05 00:27:38 +0300

    fix user.controller.js: update user model import and improve user registration logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,34 +1,37 @@
-import { user } from '../models/user.model.js';
+import User from "../models/user.model.js";
 
 const registerUser = async (req, res) => {
   try {
-    const { username, email, password } 
-     //basic validation
+    const { username, email, password } = req.body;
+
     if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // check if user already exists
-    const existingUser = await user.findOne({ email: email.toLowerCase().trim() });
+    const existingUser = await User.findOne({ email });
+
     if (existingUser) {
       return res.status(409).json({ message: "User already exists" });
     }
 
-    // create user
-    const newUser = new user({
+    const newUser = new User({
       username: username.toLowerCase().trim(),
       email: email.toLowerCase().trim(),
       password: password.trim(),
-      loggedIn: false, // In production, hash the password before saving
     });
 
-    res.status(201).json({ 
-        message: "User registered successfully",
-         user: {id: newUser._id, username: newUser.username, email: newUser.email}});
-    }
+    await newUser.save();
 
-catch (error) {
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
 
+  } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
