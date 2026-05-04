4e2d2c988a27a8d17486e34af3a804c83dfa028d
biruk2116 <brukbelay61@gmail.com>
2026-05-05 00:28:02 +0300

    fix user.route.js: standardize import statements and simplify registerUser route definition

--- backend/src/route/user.route.js
+++ backend/src/route/user.route.js
@@ -1,6 +1,8 @@
-import {Router} from 'express';
-import { registerUser } from '../controller/user.controller.js';
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controller.js";
+
 const router = Router();
 
-router.route('/register').post(registerUser);
+router.post("/register", registerUser);
+
 export default router;
