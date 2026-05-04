19c668e0cd5125bf369b1c51b15f1c72f871dc06
biruk2116 <brukbelay61@gmail.com>
2026-05-04 21:53:25 +0300

    fix user.model.js: correct syntax errors in user schema definition

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,32 +1,35 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true
-            minLength: 1,
-            maxLength: 30,
-            lowercase: true,
-            trim: true  
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-            lowercase: true,
-            trim: true,
-        },
-        password: {
-        type: String,
-        required: true,
-        minLength: 6,
-        maxLength: 50
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 1,
+      maxLength: 30,
+      lowercase: true,
+      trim: true,
     },
-    {
-        timestamps: true
-    }
-    )
 
-    export const User = mongoose.model("User", userSchema);
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+      maxLength: 50,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
