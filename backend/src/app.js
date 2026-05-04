4eb22a7097bc655d3894c559d3ac5619bb500f69
biruk2116 <brukbelay61@gmail.com>
2026-05-04 23:06:24 +0300

    refactor app.js: reorganize route imports and declarations for better structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,18 @@
 import express from 'express';
 const app = express();
 
+
+
+//routes import 
+
+import userRoutes from './route/user.route.js';
+
+// routes declaration
+app.use('/api/v1/users', userRoutes);
+app.use("/api/v1/posts",postRoutes);
+
+//example route :http://localhost:8000/api/v1/users/register
+
+
+
 export default app;
