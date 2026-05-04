15d58aefeb2cdb10d2f5d81e46b10e56d47fccc3
biruk2116 <brukbelay61@gmail.com>
2026-05-05 01:34:41 +0300

    fix user.model.js: simplify password hashing middleware by removing next() callback

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -33,12 +33,11 @@ const userSchema = new Schema(
   }
 );
 
-// 🔐 HASH PASSWORD BEFORE SAVE
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+// 🔐 HASH PASSWORD (FIXED)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 // 🔐 COMPARE PASSWORD
