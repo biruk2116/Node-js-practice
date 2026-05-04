d6059e3870ebe9bfad5a2ed951f46321e6607934
biruk2116 <brukbelay61@gmail.com>
2026-05-04 22:51:00 +0300

    add user.controller.js: implement registerUser function with basic validation

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import { user } from '../models/user.model.js';
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } 
+     //basic validation
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+}
+    catch (error) {
