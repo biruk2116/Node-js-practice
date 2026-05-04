b3520a9f880daedb3be31f0d2d3181b963ee37c8
biruk2116 <brukbelay61@gmail.com>
2026-05-04 23:19:13 +0300

    fix user.route.js: correct post method chaining for registerUser route

--- backend/src/route/user.route.js
+++ backend/src/route/user.route.js
@@ -2,5 +2,5 @@ import {Router} from 'express';
 import { registerUser } from '../controller/user.controller.js';
 const router = Router();
 
-router.post('/register').post(registerUser);
+router.route('/register').post(registerUser);
 export default router;
