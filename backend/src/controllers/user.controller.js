c8865fc36f9e2392f113036cb1c90b20a1115b5d
biruk2116 <brukbelay61@gmail.com>
2026-05-04 23:01:47 +0300

    fix user.controller.js: correct formatting and ensure proper user registration response

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -13,5 +13,24 @@ const registerUser = async (req, res) => {
     if (existingUser) {
       return res.status(409).json({ message: "User already exists" });
     }
-}
-    catch (error) {
+
+    // create user
+    const newUser = new user({
+      username: username.toLowerCase().trim(),
+      email: email.toLowerCase().trim(),
+      password: password.trim(),
+      loggedIn: false, // In production, hash the password before saving
+    });
+
+    res.status(201).json({ 
+        message: "User registered successfully",
+         user: {id: newUser._id, username: newUser.username, email: newUser.email}});
+    }
+
+catch (error) {
+
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export { registerUser };
