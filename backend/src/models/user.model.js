5a59551a9c68f094307ee2fe103ddd9fa0db06e3
biruk2116 <brukbelay61@gmail.com>
2026-05-04 21:51:51 +0300

    add user.model.js: define user schema with validation and timestamps

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true
+            minLength: 1,
+            maxLength: 30,
+            lowercase: true,
+            trim: true  
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 50
+    },
+    {
+        timestamps: true
+    }
+    )
+
+    export const User = mongoose.model("User", userSchema);
