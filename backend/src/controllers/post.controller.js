513c4131e0a0e87f281d4a91c3985050d78f89d3
biruk2116 <brukbelay61@gmail.com>
2026-05-06 00:12:07 +0300

    fix(post.controller.js): improve formatting and error handling in createPost function

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,28 +1,38 @@
 import { post as Post } from "../models/post.model.js";
 
 // create a post 
-
 const createPost = async (req, res) => {
     try {
-        const { name, description, age } =req.body;
-            if(!name || !description || !age){
-                return res.status(400).json({ message: "All fields are required" });
-            }
-        const newPost = await post.create({
+        const { name, description, age } = req.body;
+
+        if (!name || !description || !age) {
+            return res.status(400).json({ 
+                message: "All fields are required" 
+            });
+        }
+
+        const newPost = await Post.create({
             name,
             description,
             age
         });
-       res.status(201).json({
-    message: "Post created successfully",
-    data: newPost
-});
-    }
-    catch (error) {
-        res.status(500).json({ message: "Server error" });
-       
-    }
-}
 
+        res.status(201).json({
+            message: "Post created successfully",
+            data: newPost
+        });
 
+    } catch (error) {
+        console.error("🔥 CREATE POST ERROR:", error);
+
+        res.status(500).json({ 
+            message: "Server error",
+            error: error.message 
+        });
+    }
+};
+
+// read all post
+
+const getPosts = asyc (req, res) => {
 export { createPost };
