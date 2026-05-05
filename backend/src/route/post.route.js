62d979d76ff80c0cfc214be510ee4351d77726e9
biruk2116 <brukbelay61@gmail.com>
2026-05-06 00:58:23 +0300

    feat(post.route.js): add DELETE route for removing posts

--- backend/src/route/post.route.js
+++ backend/src/route/post.route.js
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import {createPost} from '../controllers/post.controller.js';
 import {updatePost} from '../controllers/post.controller.js';
 import {getPosts} from '../controllers/post.controller.js';
+import {deletePost} from '../controllers/post.controller.js';
 const router = Router();
 
 router.post('/create', createPost);
 router.get('/get', getPosts);
 router.patch('/update/:id', updatePost);
+router.delete('/delete/:id', deletePost);
 
 export default router;  
