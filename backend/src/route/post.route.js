30c8306cca9da94a7bb6acbe99211d3bd59ee268
biruk2116 <brukbelay61@gmail.com>
2026-05-06 00:20:22 +0300

    feat(post.route.js): add GET route for retrieving posts

--- backend/src/route/post.route.js
+++ backend/src/route/post.route.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import {createPost} from '../controllers/post.controller.js';
 
+import {getPosts} from '../controllers/post.controller.js';
 const router = Router();
 
 router.post('/create', createPost);
+router.get('/get', getPosts);
 
 export default router;  
