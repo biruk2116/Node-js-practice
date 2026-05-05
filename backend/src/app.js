b61bec446a18083e75213e18748fcbbf18d75631
biruk2116 <brukbelay61@gmail.com>
2026-05-05 20:38:25 +0300

    fix(app.js): remove duplicate postRoutes declaration

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 //routes import 
 
 import userRoutes from './route/user.route.js';
+import postRoutes from './route/post.route.js';
 
 // routes declaration
 app.use('/api/v1/users', userRoutes);
+app.use("/api/v1/posts",postRoutes);
 app.use("/api/v1/posts",postRoutes);
 
 //example route :http://localhost:8000/api/v1/users/register
