a9c45aa916bb1a69b496d5376340660a8443763b
biruk2116 <brukbelay61@gmail.com>
2026-05-05 20:39:07 +0300

    feat(post.route.js): add post route for creating new posts

---
+++ backend/src/route/post.route.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import {createPost} from '../controllers/post.controller.js';
+
+const router = Router();
+
+router.post('/', createPost);
+
+export default router;  
