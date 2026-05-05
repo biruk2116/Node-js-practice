02e0ed17cb0243ac1808ab0be91e51f17f3fe55d
biruk2116 <brukbelay61@gmail.com>
2026-05-05 14:27:39 +0300

    fix user.route.js: correct logoutUser route path from /logoutuser to /logout

--- backend/src/route/user.route.js
+++ backend/src/route/user.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { registerUser, loginUser,logoutUaer } from "../controllers/user.controller.js";
+import { registerUser, loginUser,logoutUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logoutuser",logoutUser);
+router.post("/logout",logoutUser);
 
 export default router;
