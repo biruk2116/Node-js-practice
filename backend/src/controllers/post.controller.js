dbfd608f1dc9373ea35ca8a2e56ec77d5ce7ff4e
biruk2116 <brukbelay61@gmail.com>
2026-05-05 20:32:33 +0300

    fix(post.controller.js): correct createPost function error handling and response structure

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -8,9 +8,17 @@ const createPost = async (req, res) => {
             if(!name || !description || !age){
                 return res.status(400).json({ message: "All fields are required" });
             }
-            
+        const post = await post.create({
+            name,
+            description,
+            age
+        });
+        res.status(201).json({
+            message: "Post created successfully",
+        });
     }
     catch (error) {
+        res.status(500).json({ message: "Server error" });
        
     }
 }
