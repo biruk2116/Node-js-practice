a5151ce6cf8d671c9bbe95e0cd33d04acc46fa8b
biruk2116 <brukbelay61@gmail.com>
2026-05-06 00:56:44 +0300

    feat(post.route.js): add PATCH route for updating posts

--- backend/src/route/post.route.js
+++ backend/src/route/post.route.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import {createPost} from '../controllers/post.controller.js';
-
+import {updatePost} from '../controllers/post.controller.js';
 import {getPosts} from '../controllers/post.controller.js';
 const router = Router();
 
 router.post('/create', createPost);
 router.get('/get', getPosts);
+router.patch('/update/:id', updatePost);
 
 export default router;  
