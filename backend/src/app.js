2cc6120c0afd0723f87f781feab5d0e275b2bed0
biruk2116 <brukbelay61@gmail.com>
2026-05-07 20:13:13 +0300

    refactor(app.js): clean up import statements and remove unnecessary comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,12 @@
-import express from 'express';
+import express from "express";
+import userRoutes from "./route/user.route.js";
+import postRoutes from "./route/post.route.js";
+
 const app = express();
 
 app.use(express.json());
 
-//routes import 
-
-import userRoutes from './route/user.route.js';
-import postRoutes from './route/post.route.js';
-
-// routes declaration
-app.use('/api/v1/users', userRoutes);
-app.use("/api/v1/posts",postRoutes);
-
-
-//example route :http://localhost:8000/api/v1/users/register
-
-
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
 
 export default app;
